721050235460532858be6102578ead0c2a03a457
antonio <antonino2002d@gmail.com>
2024-07-06 16:41:55 +0200

    Aggiunta la dipendenza da JUnit nel file .iml del progetto. Aggiunto testing JUnit di LoginUtente e RegistraUtente. Aggiunto metodo per eliminare un utente in UtenteDAO.

---
+++ Farmacia/Farmacia/src/farmacia/test/LoginUtenteTest.java
@@ -0,0 +1,78 @@
+package farmacia.test;
+
+import farmacia.controller.ControllerUtenti;
+import farmacia.database.UtenteDAO;
+import farmacia.exceptions.DBException;
+import farmacia.exceptions.LoginFailedException;
+import farmacia.exceptions.RegistrationFailedException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LoginUtenteTest {
+
+	private static ControllerUtenti controllerUtenti;
+
+	@BeforeClass
+	public static void setUp() throws ParseException {
+		controllerUtenti = ControllerUtenti.getInstance();
+		String dateString = "2023-07-06";
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date date = formatter.parse(dateString);
+		Date dataNascita = new Date(date.getTime());
+		boolean esito = true;
+		try {
+			controllerUtenti.registraCliente("user", "MiaPassword", "Utente", "Di Prova", dataNascita, "cliente@gmail.com");
+		} catch (RegistrationFailedException | DBException e) {
+			esito = false;
+		}
+		assertTrue(esito);
+	}
+
+	@AfterClass
+	public static void tearDown() throws DBException {
+		UtenteDAO utenteDAO = new UtenteDAO("user");
+		utenteDAO.deleteUtente();
+	}
+
+	@Test
+	public void testLoginUtenteRegistrato() throws DBException {
+		boolean esito = true;
+		try {
+			controllerUtenti.loginUtente("user", "MiaPassword");
+		} catch (LoginFailedException e) {
+			esito = false;
+		}
+		assertTrue(esito);
+	}
+
+	@Test
+	public void testLoginUtenteNonRegistrato() throws DBException {
+		boolean esito = true;
+		try {
+			controllerUtenti.loginUtente("user_sconosciuto", "passwd");
+		} catch (LoginFailedException e) {
+			esito = false;
+		}
+		assertFalse(esito);
+	}
+
+	@Test
+	public void testLoginUtenteRegistratoPasswordErrata() throws DBException {
+		boolean esito = true;
+		try {
+			controllerUtenti.loginUtente("user", "passwd");
+		} catch (LoginFailedException e) {
+			esito = false;
+		}
+		assertFalse(esito);
+	}
+}
---
+++ Farmacia/Farmacia/src/farmacia/test/RegistraUtenteTest.java
@@ -0,0 +1,72 @@
+package farmacia.test;
+
+import farmacia.controller.ControllerUtenti;
+import farmacia.database.UtenteDAO;
+import farmacia.exceptions.DBException;
+import farmacia.exceptions.RegistrationFailedException;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class RegistraUtenteTest {
+
+	private static ControllerUtenti controllerUtenti;
+
+	@BeforeClass
+	public static void setUp() {
+		controllerUtenti = ControllerUtenti.getInstance();
+	}
+
+	@After
+	public void tearDown() throws DBException {
+		UtenteDAO utenteDAO = new UtenteDAO("user");
+		utenteDAO.deleteUtente();
+	}
+
+	@Test
+	public void testRegistraClienteSuccess() throws ParseException {
+		String dateString = "2023-07-06";
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date date = formatter.parse(dateString);
+		Date dataNascita = new Date(date.getTime());
+		boolean esito = true;
+		try {
+			controllerUtenti.registraCliente("user", "MiaPassword", "Utente", "Di Prova", dataNascita, "cliente@gmail.com");
+		} catch (RegistrationFailedException | DBException e) {
+			esito = false;
+		}
+		assertTrue(esito);
+	}
+
+	@Test
+	public void testRegistraClienteGiaRegistrato() throws ParseException {
+		String dateString = "2023-07-06";
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date date = formatter.parse(dateString);
+		Date dataNascita = new Date(date.getTime());
+		boolean esito = true;
+		try {
+			controllerUtenti.registraCliente("user", "MiaPassword", "Utente", "Di Prova", dataNascita, "cliente@gmail.com");
+		} catch (RegistrationFailedException | DBException e) {
+			esito = false;
+		}
+		assertTrue(esito);
+
+		// provo a registrare l'utente per la seconda volta
+		try {
+			controllerUtenti.registraCliente("user", "MiaPassword", "Utente", "Di Prova", dataNascita, "cliente@gmail.com");
+		} catch (RegistrationFailedException | DBException e) {
+			esito = false;
+		}
+		assertFalse(esito);
+	}
+
+
+}
