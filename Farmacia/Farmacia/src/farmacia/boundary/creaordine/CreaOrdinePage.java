2d1958c0c8c173db88faf2d121875ce383dfed37
luigiauggiero <l.auggiero@gmail.com>
2024-07-07 12:04:32 +0200

    'finita' la creaOrdinePage ma è da rivedere il problema delle checkbox

--- Farmacia/Farmacia/src/farmacia/boundary/creaordine/CreaOrdinePage.java
+++ Farmacia/Farmacia/src/farmacia/boundary/creaordine/CreaOrdinePage.java
@@ -1,10 +1,13 @@
 package farmacia.boundary.creaordine;
 
+import farmacia.controller.ControllerCatalogo;
+import farmacia.controller.ControllerOrdini;
 import farmacia.dto.DTO;
+import farmacia.exceptions.OrderCreationFailedException;
 
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.BorderLayout;
+import java.util.*;
 
 public class CreaOrdinePage extends JFrame {
 	private JTable tblFarmaciOrdine;
@@ -12,81 +15,83 @@ public class CreaOrdinePage extends JFrame {
 	public CreaOrdinePage() {
 		setTitle("Crea Ordine");
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		setLayout(new BorderLayout());
+		setSize(800, 400);
 		setLocationRelativeTo(null);
-		pack();
-		setSize(600, 400);
 
-		CustomTableModel model = new CustomTableModel(new Object[][]{}, new String[]{"Nome", "Prezzo", "Prescrizione", "Quantità", "Possiedi la prescrizione?"});
+		CustomTableModel model = new CustomTableModel(new Object[][]{}, new String[]{"Nome", "Prezzo", "Prescrizione", "Possiedi la prescrizione?", "Quantità"});
 
 		tblFarmaciOrdine = new JTable(model);
-		tblFarmaciOrdine.getColumnModel().getColumn(4).setCellRenderer(new CheckBoxRenderer());
-		tblFarmaciOrdine.getColumnModel().getColumn(4).setCellEditor(new CheckBoxEditor());
+		tblFarmaciOrdine.getColumnModel().getColumn(3).setCellRenderer(new CheckBoxRenderer());
+		tblFarmaciOrdine.getColumnModel().getColumn(3).setCellEditor(new CheckBoxEditor());
 
 		JScrollPane scrollPane = new JScrollPane(tblFarmaciOrdine);
 		getContentPane().add(scrollPane);
 
-		tblFarmaciOrdine.getColumnModel().getColumn(3).setCellEditor(new QuantitaCellEditor());
+		tblFarmaciOrdine.getColumnModel().getColumn(4).setCellEditor(new QuantitaCellEditor());
 
-		//ControllerCatalogo controllerCatalogo = ControllerCatalogo.getInstance();
-		//List<DTO> listDTO = controllerCatalogo.visualizzaCatalogo();
-		List <DTO> listDTO = new ArrayList<>();
-		DTO DTOtest1 = new DTO();
-		DTOtest1.set("nome", "prova");
-		DTOtest1.set("prezzo", "100");
-		DTOtest1.set("prescrizione", true);
-		DTOtest1.set("quantita", 500);
-		DTOtest1.set("possiediPrescrizione", false);
-
-		DTO DTOtest2 = new DTO();
-		DTOtest2.set("nome", "prova2");
-		DTOtest2.set("prezzo", 200);
-		DTOtest2.set("prescrizione", false);
-		DTOtest2.set("quantita", 1000);
-		DTOtest2.set("possiediPrescrizione", false);
-
-		DTO DTOtest3 = new DTO();
-		DTOtest3.set("nome", "prova3");
-		DTOtest3.set("prezzo", 200);
-		DTOtest3.set("prescrizione", true);
-		DTOtest3.set("quantita", 1000);
-		DTOtest3.set("possiediPrescrizione", false);
-
-		DTO DTOtest4 = new DTO();
-		DTOtest4.set("nome", "prova4");
-		DTOtest4.set("prezzo", 200);
-		DTOtest4.set("prescrizione", false);
-		DTOtest4.set("quantita", 1000);
-		DTOtest4.set("possiediPrescrizione", false);
-
-		listDTO.add(DTOtest1);
-		listDTO.add(DTOtest2);
-		listDTO.add(DTOtest3);
-		listDTO.add(DTOtest4);
-		listDTO.add(DTOtest1);
-		listDTO.add(DTOtest1);
-		listDTO.add(DTOtest1);
-		listDTO.add(DTOtest1);
-		listDTO.add(DTOtest1);
-		listDTO.add(DTOtest1);
-		listDTO.add(DTOtest1);
+		ControllerCatalogo controllerCatalogo = ControllerCatalogo.getInstance();
+		List<DTO> listDTO = controllerCatalogo.visualizzaCatalogo();
+		List<Integer> listId = new ArrayList<>();
 
 		for(DTO farmaco : listDTO){
 			String prescrizioneNecessaria;
 			if (!((boolean) farmaco.get("prescrizione"))){
 				prescrizioneNecessaria = "-";
+				listId.add((int)farmaco.get("id"));
 				model.addRow(new Object[]{farmaco.get("nome"), farmaco.get("prezzo"),
-					prescrizioneNecessaria, farmaco.get("quantita"),
-					farmaco.get("possiediPrescrizione")});
-				model.toggleCellEditability(model.getRowCount() - 1, 4);
+					prescrizioneNecessaria, farmaco.get("possiediPrescrizione"), 0});
+				model.toggleCellEditability(model.getRowCount() - 1, 3);
 			}
 			else{
 				prescrizioneNecessaria = "Necessaria";
 				model.addRow(new Object[]{farmaco.get("nome"), farmaco.get("prezzo"),
-						prescrizioneNecessaria, farmaco.get("quantita"),
-						farmaco.get("possiediPrescrizione")});
-
+						prescrizioneNecessaria, farmaco.get("possiediPrescrizione"), 0});
 			}
 		}
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BorderLayout());
+
+		JButton btnConferma = new JButton("Conferma l'ordine");
+		JButton btnAnnulla = new JButton("Annulla l'ordine");
+
+
+		//TODO: Risolvere problema per cui la checkbox non viene correttamente letta se il suo stato viene cambiato più volte
+		buttonPanel.add(btnConferma, BorderLayout.EAST);
+		buttonPanel.add(btnAnnulla, BorderLayout.WEST);
+		btnConferma.addActionListener(e -> {
+			ControllerOrdini controllerOrdini = ControllerOrdini.getInstance();
+			Map<Integer, Integer> farmaciOrdine = new HashMap<>();
+			for (int row = 0; row < tblFarmaciOrdine.getRowCount() - 1; row++) {
+				String nome = (String)tblFarmaciOrdine.getValueAt(row, 0);
+				Float prezzo = (float)tblFarmaciOrdine.getValueAt(row, 1);
+				String necessitaPrescrizione = (String)tblFarmaciOrdine.getValueAt(row, 2);
+				Boolean possiediPrescrizione = tblFarmaciOrdine.getValueAt(row, 3) != null;
+				Integer quantita = (Integer)tblFarmaciOrdine.getValueAt(row, 4);
+				if (quantita > 0) {
+					if (necessitaPrescrizione.equals("Necessaria")) {
+						if(possiediPrescrizione)
+							farmaciOrdine.put(listId.get(row), quantita);
+						else{
+							dispose();
+							return;
+						}
+					} else {
+						farmaciOrdine.put(listId.get(row), quantita);
+					}
+				}
+			}
+			System.out.println(farmaciOrdine);
+			try {
+				controllerOrdini.creaOrdine(farmaciOrdine);
+			} catch (OrderCreationFailedException ex) {
+				farmaciOrdine.clear();
+				JOptionPane.showMessageDialog(this, ex.getMessage());
+			}
+		});
+
+		add(buttonPanel, BorderLayout.SOUTH);
+
 
 		setVisible(true);
 	}
