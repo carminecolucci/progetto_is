bee26b75ef310e93a9e8dd5aa73cb9eb412ff1bf
antonio <antonino2002d@gmail.com>
2024-07-08 21:27:29 +0200

    Finiti i javadoc di ControllerUtenti

--- Farmacia/Farmacia/src/farmacia/controller/ControllerUtenti.java
+++ Farmacia/Farmacia/src/farmacia/controller/ControllerUtenti.java
@@ -33,16 +33,47 @@ public class ControllerUtenti {
 		return uniqueInstance;
 	}
 
+	/**
+	 * Funzione che permette di registrare un nuovo <i>cliente</i> nella piattaforma a partire dai suoi dati.
+	 * @param username username dell'utente che vuole registrarsi.
+	 * @param password password dell'utente che vuole registrarsi.
+	 * @param nome nome dell'utente che vuole registrarsi.
+	 * @param cognome cognome dell'utente che vuole registrarsi.
+	 * @param dataNascita data di nascita dell'utente che vuole registrarsi.
+	 * @param email email dell'utente che vuole registrarsi.
+	 * @throws RegistrationFailedException quando <code>username</code> e/o <code>email</code> sono già presenti in
+	 * un altro utente registrato nella piattaforma.
+	 * @throws DBException
+	 */
 	public void registraCliente(String username, String password, String nome, String cognome, Date dataNascita, String email) throws RegistrationFailedException, DBException {
 		EntityFarmacia farmacia = EntityFarmacia.getInstance();
 		farmacia.registraCliente(username, password, nome, cognome, dataNascita, email);
 	}
 
+	/**
+	 * Funzione che permette ad un utente registrato di loggarsi nella piattaforma. La funzione autentica l'utente e setta
+	 * la sessione corrente del programma.
+	 * @param username username dell'utente che vuole loggarsi.
+	 * @param password password dell'utente che vuole loggarsi.
+	 * @throws LoginFailedException quando lo <code>username</code> dell'utente che cerca di loggarsi non esiste nella piattaforma
+	 * o la <code>password</code> inserita è sbagliata.
+	 * @throws DBException
+	 */
 	public void loginUtente(String username, String password) throws LoginFailedException, DBException {
 		EntityFarmacia farmacia = EntityFarmacia.getInstance();
 		farmacia.loginUtente(username, password);
 	}
 
+	/**
+	 * Funzione che ritorna l'utente attualmente loggato nel sistema. È necessario che un utente sia loggato nel sistema.
+	 * @return un'istanza di <code>DTO</code> contenente i seguenti campi:
+	 * <ul>
+	 *     <li>"nome": <code>nome</code> dell'utente attualmente loggato nel sistema.</li>
+	 *     <li>"cognome": <code>cognome</code> dell'utente attualmente loggato nel sistema.</li>
+	 *     <li>"tipoUtente": <code>TipoUtente</code> dell'utente attualmente loggato nel sistema. Può assumere i valori
+	 *     "CLIENTE", "FARMACISTA" e "DIRETTORE".</li>
+	 * </ul>
+	 */
 	public DTO getUtenteCorrente() {
 		EntityUtente utenteCorrente = Sessione.getInstance().getUtenteCorrente();
 		DTO dto = new DTO();
