9bb9792be7c67658ec74febd26091d6be359d56c
antonio <antonino2002d@gmail.com>
2024-07-08 21:16:31 +0200

    Completati i javadoc di ControllerOrdini

--- Farmacia/Farmacia/src/farmacia/controller/ControllerOrdini.java
+++ Farmacia/Farmacia/src/farmacia/controller/ControllerOrdini.java
@@ -51,7 +51,9 @@ public class ControllerOrdini {
 	 *     <li>"id": <code>id</code> dell'ordine (<code>String</code>)</li>
 	 *     <li>"dataCreazione": <code>dataCreazione</code> dell'ordine (<code>Date</code>)</li>
 	 *     <li>"ritirato": <code>ritirato</code> dell'ordine (<code>boolean</code>)</li>
-	 *     <li>"quantitaFarmaci": <code>quantitaFarmaci</code> dell'ordine (<code>DTO</code>)</li>
+	 *     <li>"quantitaFarmaci": <code>quantitaFarmaci</code> dell'ordine (<code>Map&lt;DTO, Integer&gt;</code>). La chiave
+	 *     di questo <code>Map</code> è un'istanza di <code>DTO</code> rappresentante un farmaco.</li>
+	 *     <li>"cliente": <code>cliente</code> titolare dell'ordine (<code>String</code>)</li>
 	 *     <li>"totale": <code>totale</code> dell'ordine (<code>float</code>)</li>
 	 * </ul>
 	 * @throws DBException
@@ -81,11 +83,31 @@ public class ControllerOrdini {
 		return dtoQuantitaFarmaci;
 	}
 
+	/**
+	 * Funzione che permette di creare un ordine di acquisto. Viene richiamata quando un ordine da parte di un cliente
+	 * azzera le scorte di un determinato farmaco ({@link #creaOrdine(Map)}) o quando un farmacista decide di generare
+	 * un ordine di acquisto.
+	 * @param farmaciQuantita mappa che contiene una serie di coppie (<code>idFarmaco</code>, <code>quantità</code>).
+	 * @return l'<code>id</code> dell'ordine appena creato.
+	 * @throws OrderCreationFailedException quando la creazione dell'ordine fallisce
+	 */
 	public void creaOrdineAcquistoFarmacia(Map<Integer, Integer> farmaciQuantita) throws DBException, OrderCreationFailedException {
 		EntityFarmacia farmacia = EntityFarmacia.getInstance();
 		farmacia.creaOrdineAcquisto(farmaciQuantita);
 	}
 
+	/**
+	 * Funzione che permette di ottenere tutti gli ordini di acquisto memorizzati nella farmacia.
+	 * @return una <code>List&lt;DTO&gt;</code>, in cui ogni istanza di <code>DTO</code> contiene un singolo ordine di acquisto.
+	 * In particolare il <code>DTO</code> contiene i seguenti campi:
+	 * <ul>
+	 *     <li>"id": <code>id</code> dell'ordine di acquisto (<code>String</code>)</li>
+	 *     <li>"dataCreazione": <code>dataCreazione</code> dell'ordine di acquisto (<code>Date</code>)</li>
+	 *     <li>"ricevuto": <code>ricevuto</code> dell'ordine di acquisto (<code>boolean</code>)</li>
+	 *     <li>"quantitaFarmaci": <code>quantitaFarmaci</code> dell'ordine di acquisto (<code>Map&lt;DTO, Integer&gt;</code>)</li>
+	 * </ul>
+	 * @throws DBException
+	 */
 	public List<DTO> visualizzaOrdiniAcquistoFarmacia() throws DBException {
 		EntityFarmacia farmacia = EntityFarmacia.getInstance();
 		List<DTO> dtoOrdiniAcquisto = new ArrayList<>();
@@ -104,12 +126,43 @@ public class ControllerOrdini {
 		return dtoOrdiniAcquisto;
 	}
 
+	/**
+	 * Funzione che permette di ottenere tutti gli ordini di tutti i clienti.
+	 * @return una <code>List&lt;DTO&gt;</code>, in cui ogni istanza di <code>DTO</code> contiene un singolo ordine di un cliente.
+	 * In particolare il <code>DTO</code> contiene i seguenti campi:
+	 * <ul>
+	 *     <li>"id": <code>id</code> dell'ordine (<code>String</code>)</li>
+	 *     <li>"dataCreazione": <code>dataCreazione</code> dell'ordine (<code>Date</code>)</li>
+	 *     <li>"ritirato": <code>ritirato</code> dell'ordine (<code>boolean</code>)</li>
+	 *     <li>"quantitaFarmaci": <code>quantitaFarmaci</code> dell'ordine (<code>Map&lt;DTO, Integer&gt;</code>). La chiave
+	 *     di questo <code>Map</code> è un'istanza di <code>DTO</code> rappresentante un farmaco.</li>
+	 *     <li>"cliente": <code>cliente</code> titolare dell'ordine (<code>String</code>)</li>
+	 *     <li>"totale": <code>totale</code> dell'ordine (<code>float</code>)</li>
+	 * </ul>
+	 * @throws DBException
+	 */
 	public List<DTO> visualizzaOrdiniFarmacia() throws DBException {
 		EntityFarmacia farmacia = EntityFarmacia.getInstance();
 		List<EntityOrdine> ordini = farmacia.visualizzaOrdini();
 		return getDtoOrdini(ordini);
 	}
 
+	/**
+	 * Funzione privata di utilità che converte una <code>List&lt;EntityOrdine&gt;</code> in una <code>List&lt;DTO&gt;</code>.
+	 * @param ordini
+	 @return una <code>List&lt;DTO&gt;</code>, in cui ogni istanza di <code>DTO</code> contiene un singolo ordine di un cliente.
+	  * In particolare il <code>DTO</code> contiene i seguenti campi:
+	  * <ul>
+	  *     <li>"id": <code>id</code> dell'ordine (<code>String</code>)</li>
+	  *     <li>"dataCreazione": <code>dataCreazione</code> dell'ordine (<code>Date</code>)</li>
+	  *     <li>"ritirato": <code>ritirato</code> dell'ordine (<code>boolean</code>)</li>
+	  *     <li>"quantitaFarmaci": <code>quantitaFarmaci</code> dell'ordine (<code>Map&lt;DTO, Integer&gt;</code>). La chiave
+	  *     di questo <code>Map</code> è un'istanza di <code>DTO</code> rappresentante un farmaco.</li>
+	  *     <li>"cliente": <code>cliente</code> titolare dell'ordine (<code>String</code>)</li>
+	  *     <li>"totale": <code>totale</code> dell'ordine (<code>float</code>)</li>
+	  * </ul>
+	 * @throws DBException
+	 */
 	private List<DTO> getDtoOrdini(List<EntityOrdine> ordini) throws DBException {
 		List<DTO> dtoOrdini = new ArrayList<>();
 		for (EntityOrdine ordine : ordini) {
@@ -126,11 +179,23 @@ public class ControllerOrdini {
 		return dtoOrdini;
 	}
 
+	/**
+	 * Funzione che cambia lo stato di un ordine da 'Non ritirato' a 'Ritirato' cercandolo con il suo <code>id</code>.
+	 * @param idOrdine viene usato per la ricerca dell'ordine.
+	 * @throws OrderNotFoundException quando non esiste un ordine con <code>id</code> uguale a <code>idOrdine</code>.
+	 * @throws DBException
+	 */
 	public void aggiornaOrdine(String idOrdine) throws OrderNotFoundException, DBException {
 		EntityFarmacia farmacia = EntityFarmacia.getInstance();
 		farmacia.aggiornaOrdine(idOrdine);
 	}
 
+	/**
+	 * Funzione che cambia lo stato di un ordine di acquisto da 'Non ritirato' a 'Ritirato' cercandolo con il suo <code>id</code>.
+	 * @param idOrdine viene usato per la ricerca dell'ordine di acquisto.
+	 * @throws OrderNotFoundException quando non esiste un ordine di acquisto con <code>id</code> uguale a <code>idOrdine</code>.
+	 * @throws DBException
+	 */
 	public void aggiornaOrdineAcquisto(String idOrdine) throws OrderNotFoundException, DBException {
 		EntityFarmacia farmacia = EntityFarmacia.getInstance();
 		farmacia.aggiornaOrdineAcquisto(idOrdine);
