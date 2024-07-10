1986ab0afed9fb60bd436f463eae3549f097dbc6
antonio <antonino2002d@gmail.com>
2024-07-10 12:28:31 +0200

    Iniziato ad aggiungere il test strutturale

---
+++ docs/chapters/testing_white_box/creaOrdine.java
@@ -0,0 +1,30 @@
+public String creaOrdine(Map<Integer, Integer> farmaciQuantita) throws OrderCreationFailedException {
+	EntityCatalogo catalogo = EntityCatalogo.getInstance();
+	if (!catalogo.checkScorte(farmaciQuantita)) {
+		throw new OrderCreationFailedException("Ordine non creato per mancanza scorte");
+	}
+	EntityOrdine ordine = new EntityOrdine(this.getId());
+	try {
+		EntityOrdineAcquisto ordineAcquisto = new EntityOrdineAcquisto();
+		for (Map.Entry<Integer, Integer> entry : farmaciQuantita.entrySet()) {
+			int id = entry.getKey();
+			int quantita = entry.getValue();
+			EntityFarmaco farmaco = catalogo.cercaFarmacoById(id);
+			ordine.aggiungiOrdineFarmaco(farmaco, quantita);
+			int scorteResidue = catalogo.decrementaScorte(id, quantita);
+			if (scorteResidue == 0) {
+				ordineAcquisto.aggiungiOrdineAcquistoFarmaco(farmaco, EntityOrdineAcquisto.QUANTITA_ORDINE_DEFAULT);
+			}
+		}
+		if (!ordineAcquisto.getQuantitaFarmaci().isEmpty()) {
+			ordineAcquisto.salvaInDB();
+		}
+		ordine.salvaInDB();
+		storicoOrdini.add(ordine);
+		return ordine.getId();
+	} catch (FarmacoNotFoundException e) {
+		throw new OrderCreationFailedException("Errore creazione ordine, farmaco non trovato");
+	} catch (DBException e) {
+		throw new OrderCreationFailedException(e.getMessage());
+	}
+}
